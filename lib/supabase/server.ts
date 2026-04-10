82e2fc9956b976da3e8e46ffa1b34a10dfe43b4e
chavesfilip <chavesfilip@gmail.com>
2026-04-10 21:36:24 +0000

    mudança nos arquivos

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -12,14 +12,14 @@ export async function createClient() {
         getAll() {
           return cookieStore.getAll()
         },
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: any) {
           try {
-            cookiesToSet.forEach(({ name, value, options }) =>
+            cookiesToSet.forEach(({ name, value, options }: any) =>
               cookieStore.set(name, value, options)
             )
           } catch {
-            // The `setAll` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing sessions.
+            // O método `setAll` foi chamado de um Server Component.
+            // Isto pode ser ignorado se tiveres o middleware a atualizar as sessões.
           }
         },
       },
